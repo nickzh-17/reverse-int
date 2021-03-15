b83a52b834676cc0a084d67e10086cfee5c68aab
nikolayzhnr <mikalayzh@gmail.com>
2021-03-15 20:41:55 +0300

    reverse int task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
   
+    // Преобразования N в строку, затем поэлементно в массив 
+    let nString = n.toString().split('');
+
+
+    //Проверка есть ли минус в первой ячейке. Если есть - удаляем его
+    if(nString[0] == '-'){
+        nString.splice(0,1);
+    }
+
+    // Переворот массива, обращение в строку без разделителей, обращение строки в число 
+    return parseFloat(nString.reverse().join(''));
+
 }
